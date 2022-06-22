ff18abaad6400c07ad240de7f8b0520d054b2c06
leticiamrosa <leticiamonteiro.design@gmail.com>
2022-06-22 18:39:15 -0300

    feat: apply baseUrl for api v1

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 
 import PokemonsRouter from '@routes/PokemonsRouter/PokemonsRouter'
-const routes = Router()
 
-routes.use('/pokemons', PokemonsRouter)
+const routes = Router()
+const baseUrl = '/api/v1'
+
+routes.use(`${baseUrl}/pokemons`, PokemonsRouter)
 
 export default routes
